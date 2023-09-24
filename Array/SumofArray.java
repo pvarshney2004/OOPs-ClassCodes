ef6cd0f6706a28b92f946a545b1486ab9ddddcca
Prashant <prashantvarshney4321@gmail.com>
2023-09-25 02:20:12 +0530

    ClassCodes

--- Array/SumofArray.java
+++ Array/SumofArray.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Array;
 
 public class SumofArray {
@@ -11,17 +10,4 @@ public class SumofArray {
         System.out.println(sum);
     }
 }
-=======
-package Array;
 
-public class SumofArray {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int sum=0;
-        for(int i=0; i<arr.length; i++){
-            sum+=arr[i];
-        }
-        System.out.println(sum);
-    }
-}
->>>>>>> 249ce122c101b0b13fc3f1e04892fe9c0529059d
---
+++ newPattern.java
@@ -0,0 +1,5 @@
+public class newPattern {
+    public static void main(String[] args) {
+        System.out.println("It is a demo file for git hub");
+    }
+}
