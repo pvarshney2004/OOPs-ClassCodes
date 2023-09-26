7d80f0f45860c204459d9a91e63d5f835a6307fb
Prashant <prashantvarshney4321@gmail.com>
2023-09-26 11:55:26 +0530

    ClassCodes

---
+++ RemoveDupChar.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class RemoveDupChar {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String str = "aabacbn";
+        String newstr = removeDupChar(str);
+        System.out.println(newstr);
+    }
+
+    public static String removeDupChar(String str) {
+        String newstr="";
+        for(int i=0; i<str.length(); i++){
+            boolean isDup=false;
+            for(int j=0; j<newstr.length(); j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    isDup=true;
+                    break;
+                }
+            }
+            if(!isDup){
+                newstr+=str.charAt(i);
+            }
+        }
+        return newstr;
+    }
+}
---
+++ RemoveSpeChar.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RemoveSpeChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        String newstr = str.replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(newstr);
+
+        System.out.println("Hello World!");
+
+    }
+}
